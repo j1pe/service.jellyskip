7d20af0b65e21aeabe2d55710d2f7dcba77bc7a1
j1pe <32877747+j1pe@users.noreply.github.com>
2026-04-02 14:22:22 +0200

    Add files via upload

--- resources/lib/skip_dialogue.py
+++ resources/lib/skip_dialogue.py
@@ -6,16 +6,17 @@ import helper.utils as utils
 from helper import LazyLogger
 
 OK_BUTTON = 2101
-
-INSTRUCTION_LABEL = 203
-AUTHCODE_LABEL = 204
-WARNING_LABEL = 205
-CENTER_Y = 6
-CENTER_X = 2
+QUIT_BUTTON = 2102
 
 MIN_REMAINING_SECONDS = 5
-MAX_DISPLAY_SECONDS = 6  # Maximum time to show the skip button before auto-hide
+MAX_DISPLAY_SECONDS = 10  # Temps d'affichage max de l'intro
+
 LOG = LazyLogger(__name__)
+
+# --- PARAMÈTRES DE BINGE WATCHING ---
+MAX_BINGE_EPISODES = 3
+BINGE_TIMEOUT_SECONDS = 10
+BINGE_PROPERTY_KEY = "jellyskip_binge_count"
 
 class SkipSegmentDialogue(xbmcgui.WindowXMLDialog):
 
@@ -25,24 +26,19 @@ class SkipSegmentDialogue(xbmcgui.WindowXMLDialog):
         self.player = xbmc.Player()
         self.is_initial_play = is_initial_play
         self.play_start_time = play_start_time
+        self.is_closed = False  # Sécurité pour stopper la boucle du compteur
 
     def onInit(self):
-        # Read setting here (not at module level) so changes take effect without restart
         autoskip = xbmcaddon.Addon('service.jellyskip').getSettingBool('autoskip')
         if autoskip:
-            # Close dialog immediately to prevent visual artifacts
             self.close()
-            # Delay 5 seconds on initial play to let TV sync/blank settle
-            # Only apply delay if we're within 15 seconds of pressing play in Kodi UI
             if self.is_initial_play and (time.time() - self.play_start_time) < 15:
                 xbmc.sleep(5000)
             xbmc.executebuiltin('Notification(Jellyskip, Skipped %s, 3000)' % self.segment_type)
-            # Do the seek directly instead of going through onClick
             if self.player.isPlaying():
                 current_time = self.player.getTime()
                 total_time = self.player.getTotalTime()
                 skip_distance = self.seek_time_seconds - current_time
-                # Only seek if target is ahead and skip distance is meaningful (> 5s)
                 if total_time > 0 and skip_distance > 5:
                     from dialogue_handler import dialogue_handler
                     dialogue_handler.autoskip_time = time.time()
@@ -52,36 +48,94 @@ class SkipSegmentDialogue(xbmcgui.WindowXMLDialog):
                     else:
                         self.player.seekTime(self.seek_time_seconds)
             return
-        skip_label = 'Skip ' + str(self.segment_type)
-        skip_button = self.getControl(OK_BUTTON)
-        skip_button.setLabel(skip_label)
+
+        quit_button = self.getControl(QUIT_BUTTON)
+        if self.segment_type in ["Outro", "Credits"]:
+            quit_button.setVisible(True)
+        else:
+            quit_button.setVisible(False)
+
         self.schedule_close_action()
 
     def get_seconds_till_segment_end(self):
         return self.seek_time_seconds - self.player.getTime()
 
     def schedule_close_action(self):
-        """
-        Schedule the dialog to close after MAX_DISPLAY_SECONDS or when segment ends, whichever is sooner.
-        :return: None
-        """
+        segment_time_remaining = self.get_seconds_till_segment_end()
 
-        seconds_till_segment_end = self.get_seconds_till_segment_end()
-        display_time = min(seconds_till_segment_end, MAX_DISPLAY_SECONDS)
+        if self.segment_type in ["Outro", "Credits"]:
+            display_time = BINGE_TIMEOUT_SECONDS
+        else:
+            display_time = min(segment_time_remaining, MAX_DISPLAY_SECONDS)
 
         if display_time > 0:
-            utils.run_threaded(self.on_automatic_close, delay=display_time, kwargs={})
+            # On lance la boucle de mise à jour dynamique au lieu du simple délai
+            utils.run_threaded(self.countdown_loop, delay=0, kwargs={'start_time': int(display_time)})
+        else:
+            utils.run_threaded(self.on_automatic_close, delay=15, kwargs={})
+
+    def countdown_loop(self, start_time):
+        current_time = start_time
+        
+        # Lecture de la mémoire pour l'Outro
+        window = xbmcgui.Window(10000)
+        current_count_str = window.getProperty(BINGE_PROPERTY_KEY)
+        current_count = int(current_count_str) if current_count_str else 0
+        is_limit_reached = current_count >= MAX_BINGE_EPISODES
+
+        while current_time > 0 and not self.is_closed:
+            try:
+                skip_button = self.getControl(OK_BUTTON)
+                if self.segment_type in ["Outro", "Credits"]:
+                    if is_limit_reached:
+                        skip_button.setLabel(f"Veille. Arrêt dans {current_time}s")
+                    else:
+                        skip_button.setLabel(f"Épisode suivant ({current_time}s)")
+                else:
+                    skip_button.setLabel(f"Skip {self.segment_type} ({current_time}s)")
+            except:
+                break  # Si la fenêtre a été détruite brutalement
+
+            # On attend 1 seconde en vérifiant 10 fois si l'utilisateur a fermé le menu
+            for _ in range(10):
+                if self.is_closed:
+                    break
+                xbmc.sleep(100)
+            
+            current_time -= 1
+
+        # Si le temps arrive à zéro et que l'utilisateur n'a rien touché, on déclenche l'action auto
+        if not self.is_closed:
+            self.on_automatic_close()
+
+    def reset_binge_counter(self):
+        window = xbmcgui.Window(10000)
+        window.clearProperty(BINGE_PROPERTY_KEY)
 
     def on_automatic_close(self):
-        """
-        Close the dialog automatically. This is called by the scheduled thread.
-        :return: None
-        """
+        self.is_closed = True
+        
+        if self.segment_type in ["Outro", "Credits"] and self.player.isPlaying():
+            window = xbmcgui.Window(10000)
+            current_count_str = window.getProperty(BINGE_PROPERTY_KEY)
+            current_count = int(current_count_str) if current_count_str else 0
+
+            if current_count < MAX_BINGE_EPISODES:
+                current_count += 1
+                window.setProperty(BINGE_PROPERTY_KEY, str(current_count))
+                xbmc.executebuiltin('PlayerControl(Next)')
+            else:
+                self.reset_binge_counter()
+                self.player.stop()
+                xbmc.executebuiltin('Notification(Jellyskip, Lecture suspendue pour inactivité, 5000)')
+
         self.close()
         xbmc.executebuiltin("NotifyAll(%s, %s, %s)" % ("service.jellyskip", "Jellyskip.DialogueClosed", {}))
 
     def onAction(self, action):
         if action in (ACTION_NAV_BACK, ACTION_PREVIOUS_MENU, ACTION_STOP):
+            self.is_closed = True
+            self.reset_binge_counter()
             self.close()
 
     def onControl(self, control):
@@ -91,18 +145,25 @@ class SkipSegmentDialogue(xbmcgui.WindowXMLDialog):
         pass
 
     def onClick(self, control):
+        self.is_closed = True
+        
         if not self.player.isPlaying():
             self.close()
             return
 
-        if control == OK_BUTTON:
-            remaining_seconds = self.player.getTotalTime() - self.seek_time_seconds
+        self.reset_binge_counter()
 
-            # We don't want to skip to the end of the video (give other addons time to play, like nextup service)
-            if remaining_seconds < MIN_REMAINING_SECONDS:
-                self.player.seekTime(self.player.getTotalTime() - MIN_REMAINING_SECONDS)
-                self.close()
+        if control == OK_BUTTON:
+            if self.segment_type in ["Outro", "Credits"]:
+                xbmc.executebuiltin('PlayerControl(Next)')
             else:
-                self.player.seekTime(self.seek_time_seconds)
+                remaining_seconds = self.player.getTotalTime() - self.seek_time_seconds
+                if remaining_seconds < MIN_REMAINING_SECONDS:
+                    self.player.seekTime(self.player.getTotalTime() - MIN_REMAINING_SECONDS)
+                else:
+                    self.player.seekTime(self.seek_time_seconds)
+                    
+        elif control == QUIT_BUTTON:
+            self.player.stop()
 
         self.close()
